37fc7efa0c1a6f5e794d4e584a58b567fd026300
fabbrice2 <fabriceamenouglo@gmail.com>
2024-02-07 14:17:55 +0100

    Merge remote changes

--- todolistfrontend/src/App.tsx
+++ todolistfrontend/src/App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import TaskList from "./components/TaskList";
-import AddTask from "./components/AddTask";
 
 const App: React.FC = () => {
   return (
     <div className="bg-[#2A2B2F] h-screen text-white">
       <Header />
       <Banner />
-      <Routes>
-        <Route path="/addtask" element={<AddTask />} />
-        <Route path="/" element={<TaskList />} />
-      </Routes>
+
+      <TaskList />
+      {/* <TaskForm /> */}
     </div>
   );
 };
